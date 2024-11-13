f7edf367635abd0b3f975a465d39ad11e08205d8
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-11-13 20:30:34 +0200

    Install flowbite, add carousel to show note page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default withMT ({
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
@@ -19,5 +20,8 @@ export default withMT ({
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
 });
